d08681ed4063871b154ab83add8ff528dc77481e
chaymahamouda <chaymahamouda93@gmail.com>
2022-07-03 16:52:27 +0200

    first commit

---
+++ api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserList from './components/UserList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
